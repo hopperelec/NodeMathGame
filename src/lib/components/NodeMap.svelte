96553792225adb78bb31fb774b5331b609a76cda
hopperelec <hopperelec@gmail.com>
2024-10-16 02:16:00 +0100

    Make terminating nodes (e.g: sell nodes) get executed every second

--- src/lib/components/NodeMap.svelte
+++ src/lib/components/NodeMap.svelte
@@ -9,13 +9,17 @@ function parseAnchorId(anchorId: { id: string }): number {
 	return +anchorId.id.split("/", 1)[0].substring(2);
 }
 
+function isTerminus(node: PlacedNode): boolean {
+    return node.outputs.length === 0;
+}
+
 function onConnection(event: CustomEvent) {
 	const sourceNode = nodes[event.detail.sourceNode.id.substring(2)];
 	const sourceOutputId = parseAnchorId(event.detail.sourceAnchor);
 	const targetNode = nodes[event.detail.targetNode.id.substring(2)];
 	const targetInputId = parseAnchorId(event.detail.targetAnchor);
 	targetNode.inputs[targetInputId] = sourceNode.outputs[sourceOutputId];
-	if (targetNode.inputs.every((input) => input !== null)) {
+	if (targetNode.inputs.every((input) => input !== null) && !isTerminus(targetNode)) {
 		targetNode.outputs = targetNode.type.processor(targetNode.inputs);
 	}
 }
@@ -26,6 +30,13 @@ function onDisconnection(event: CustomEvent) {
 	targetNode.inputs[targetInputId] = null;
 	targetNode.outputs = targetNode.outputs.map(() => null);
 }
+
+setInterval(() => {
+    for (const node of nodes) {
+        if (node.inputs.every((input) => input !== null) && isTerminus(node))
+            node.type.processor(node.inputs);
+    }
+}, 1000);
 </script>
 
 <Svelvet minimap controls pannable theme="dark" edgesAboveNode on:connection={onConnection} on:disconnection={onDisconnection}>
